05adc9d57fc25ecc7c0dac3e69880b0bf1e3efce
Alfredo <aarma-028@gmail.com>
2023-05-05 13:43:08 -0400

    Styling has been applied and modal is ready

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Modal from "./components/Modal/Modal";
 import React, { useState } from "react";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        Fancy Modal
-      </Modal>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}></Modal>
     </div>
   );
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 import "../Modal/Modal.scss";
-// import { ReactComponent as X } from "../../assets/icons/close-24px.svg";
+import Exit from "../../Assets/Exit.png";
 
 function WarehouseModal({ open, onClose, handler }) {
-  if (!open) return null;
-
   return ReactDom.createPortal(
     <div className="overlayStyle">
       <div className="modalStyle">
-        {/* <X onClick={onClose} /> */}
         <div className="modalContainer">
+          <span onClick={onClose}>
+            <img src={Exit} alt="close window" className="modal__close" />
+          </span>
           <h1 className="modalTitle">Event Review</h1>
+
           <p className="modalText">
             Please take a moment to leave a review for last month's team event.
             Your feedback helps us plan better building opportunities for the
             team.
           </p>
           <div className="buttonContainer">
-            <button onClick={onClose} className="modalButton">
-              Remind Me Later
+            <button onClick={onClose} className="modalButton modalButton-close">
+              <p className="modalButton__text--close">Remind Me Later</p>
             </button>
             <button
               onClick={handler}
-              className="modalButton modalButton-delete"
+              className="modalButton modalButton-survey"
             >
-              Yes
+              <p className="modalButton__text--survey">Ok</p>
             </button>
           </div>
         </div>
